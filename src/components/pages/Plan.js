34c7765a2a8ffc86c7be7e0cf93633397652c8f2
stormning <stormning@163.com>
2017-06-13 13:57:38 +0800

    Plan.js

--- src/components/pages/Plan.js
+++ src/components/pages/Plan.js
@@ -4,7 +4,7 @@ import MediaCard from '../ui/MediaCard';
 const Plan = ({plans = []}) => {
     // console.log(plans);
     const cards = plans.map((plan) => (
-        <MediaCard />
+        <MediaCard key={plan.id}/>
     ));
     return <div className="cards-container">{cards}</div>
 };
--- src/components/ui/MediaCard.js
+++ src/components/ui/MediaCard.js
@@ -1,45 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles, createStyleSheet } from 'material-ui/styles';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import {withStyles, createStyleSheet} from 'material-ui/styles';
+import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import reptileImage from '../../../assets/images/plan.jpg';
+import '../../../assets/css/ui/mediacard.scss';
 
-const styleSheet = createStyleSheet('SimpleMediaCard', {
-    card: {
-        maxWidth: 345,
-    },
-});
-
-function MediaCard(props) {
-    const classes = props.classes;
-    return (
-        <div>
-            <Card className={classes.card}>
-                <CardMedia>
-                    <img src={reptileImage} alt="Contemplative Reptile" />
-                </CardMedia>
-                <CardContent>
-                    <Typography type="headline" component="h2">
-                        Lizard
-                    </Typography>
-                    <Typography component="p">
-                        Lizards are a widespread group of squamate reptiles, with over
-                        6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button compact primary>Share</Button>
-                    <Button compact primary>Learn More</Button>
-                </CardActions>
-            </Card>
-        </div>
+const MediaCard = ({content, free}) =>
+    (
+        <Card className='media-card'>
+            <CardMedia>
+                <img src={reptileImage} alt="Contemplative Reptile"/>
+            </CardMedia>
+            <CardContent>
+                <Typography type="headline" component="h2">
+                    Lizard
+                </Typography>
+                <Typography component="p">
+                    Lizards are a widespread group of squamate reptiles, with over
+                    6,000 species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button compact primary>升级</Button>
+            </CardActions>
+        </Card>
     );
-}
-
-MediaCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styleSheet)(MediaCard);
+export default MediaCard;
