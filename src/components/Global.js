150289950c37246c118cfbd2ec84fb1eac27ae55
stormning <stormning@163.com>
2018-02-09 15:57:25 +0800

    mock api url

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://www.easy-mock.com/mock/593e2ea18ac26d795fe6055a/API';
+axios.defaults.baseURL = 'https://www.easy-mock.com/mock/59af58aee0dc6633419e2413/api_1504663726087';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
